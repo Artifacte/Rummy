3aa54c7093aa5d07aeaa1fafbf19aaf6897237b8
medhagupta <test@L-MAA-25004064.paypalcorp.com>
2015-07-30 18:33:40 +0530

    added member functions for Rummy class

--- Rummy.java
+++ Rummy.java
@@ -6,4 +6,26 @@ public class Rummy {
 		return 0;
 	}
 	
+	private List<Set> getAllPossible3LengthPermutations(List<Integer> hand){
+		return null;
+	}
+	
+	private List<Set> getAllPossible4LengthPermutations(List<Integer> hand){
+		return null;
+	}
+	
+	private List<Set> getAllPossible5LengthPermutations(List<Integer> hand){
+		return null;
+	}
+	
+	private int getNumberForCard(String card){
+		return 0;
+	}
+	
+	private int findUnusedCards(List<Integer> leftCards ,boolean areAtleastTwoSequencesFound , boolean isTripletorQuadFound ){
+		
+		return 0;
+	
+	}
 }
+	
