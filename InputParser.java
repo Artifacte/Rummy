a0c1e15a8380dd452827f99be710b397b19f8263
Artifacte <rajat.kulshreshtah50@gmail.com>
2015-07-30 18:58:54 +0530

    The parser file to take cards in user hand

--- InputParser.java
+++ InputParser.java
@@ -1,8 +1,22 @@
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.*;
 
 public class InputParser {
-
-	public static void main(String[] args){
-		//parses the input and calls the rummy API
-	}
-	
+    public static void main(String[] args) throws FileNotFoundException, IOException {
+        try (Scanner reader = new Scanner(System.in)) {
+            System.out.println("Enter the 13 cards in hand: (e.g. 8H) ");
+            String file = reader.nextLine();
+            BufferedReader br = new BufferedReader(new FileReader(".\\src\\" + file));
+            List<String> cardsInHand = new ArrayList<String>() {};
+            for (int i = 0; i < 13; i++){
+                String s = br.readLine();
+                cardsInHand.add(s);
+            }
+            System.out.println(cardsInHand.get(12));
+            reader.close();
+        }
+    }    
 }
